729b3ede0deab823a3a2518cea4136f218868d47
polemarch <polemarch@192.168.1.159>
2013-10-05 22:41:23 +0300

    begin core project

--- index.php
+++ index.php
@@ -11,4 +11,14 @@
 
 
 header('Content-type: text/html');
+
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+date_default_timezone_set('Europe/Istanbul');
+
+$betik_zd = date_default_timezone_get();
+$q = isset($_GET ['q']) ? $_GET ['q'] : null;
+$q = rtrim($q, '/');
+$q = explode('/', $q);
+print_r($q);
 ?>
