d868c2d1af582a9d971597df2ae5e736b69610a5
polemarch <polemarch@192.168.1.159>
2013-10-06 00:17:07 +0300

    begin core project

--- index.php
+++ index.php
@@ -14,10 +14,7 @@ header('Content-type: text/html');
 
 @ini_set("display_errors", 1);
 @ini_set( 'error_reporting', E_ALL ); 
-
-
 date_default_timezone_set('Europe/Istanbul');
-
 $q = isset($_GET ['q']) ? $_GET ['q'] : null;
 $q = rtrim($q, '/');
 $q = explode('/', $q);
